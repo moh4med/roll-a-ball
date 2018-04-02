404820ee483150061fd23ee097450f77cf69a863
mohamed <mohamed.ahmed@excelcodes.com>
2018-04-02 19:20:57 +0200

    setup the play area and moving the camera with player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,7 +15,6 @@ public class PlayerController : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        //Debug.Log(movement * speed);
         rb.AddForce(movement * speed);
     }
 }
